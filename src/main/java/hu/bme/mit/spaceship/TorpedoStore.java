fa3d3457f1e663d685351ac7cfd39a52000a91f4
pethesdaniel <16624998+pethesdaniel@users.noreply.github.com>
2022-04-22 11:08:48 +0200

    Fixing SolarLint errors in TorpedoStore.java

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.spaceship;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -13,6 +15,8 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
+
+  private Random generator = getRandomGenerator();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -28,20 +32,27 @@ public class TorpedoStore {
     }
   }
 
+  private Random getRandomGenerator() {
+    try{
+      return SecureRandom.getInstanceStrong();
+    } catch(NoSuchAlgorithmException e) {
+      throw new IllegalStateException("Couldn't get random generator!");
+    }
+  }
+
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
