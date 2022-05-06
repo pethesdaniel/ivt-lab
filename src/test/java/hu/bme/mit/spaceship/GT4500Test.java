81a95f8064e7b9d5723ba3def83d27eba6456ccf
pethesdaniel <16624998+pethesdaniel@users.noreply.github.com>
2022-05-06 10:58:43 +0200

    Change preexisting tests to use mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,34 +7,40 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
+  private TorpedoStore mockStorePrimary = mock(TorpedoStore.class);
+  private TorpedoStore mockStoreSecondary = mock(TorpedoStore.class);
 
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    this.ship = new GT4500(mockStorePrimary, mockStoreSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockStorePrimary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockStorePrimary, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockStorePrimary.fire(1)).thenReturn(true);
+    when(mockStoreSecondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockStorePrimary, times(1)).fire(1);
+    verify(mockStoreSecondary, times(1)).fire(1);
   }
 
 }
